85d782019256285100a6399b8814dff19e02b349
nathan <nathan.mamalyga@keyapp.top>
2026-03-16 15:43:23 +0200

    finished

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,7 @@
 import flatpickr from "flatpickr";
 import iziToast from "izitoast";
 import "flatpickr/dist/flatpickr.min.css";
+import "izitoast/dist/css/iziToast.min.css";
 
 const datetimePicker = document.getElementById("datetime-picker");
 const startButton = document.querySelector(".timer-button");
